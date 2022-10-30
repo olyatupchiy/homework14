01676ed6d77d948c6c3d95b28f21ffc75016ce40
olyatupchiy <olya.tupchiy@gmail.com>
2022-10-30 12:15:33 +0100

    Added object Store

---
+++ index.js
@@ -0,0 +1,33 @@
+const store = {
+    name: 'Product store',
+    address: {
+        country: 'Poland',
+        city: 'Cracow',
+        street: 'Florianska',
+        houseNumber: 25,
+    },
+};
+store.clients = {};
+store.clients['Olha Tupchii'] = {
+    quantity: 1,
+    bonuses: 3,
+};
+store.clients['Yuriy Duchev'] = {
+    quantity: 3,
+    bonuses: 14,
+};
+store.clients['Viacheslav Koniev'] = {
+    quantity: 2,
+    bonuses: 43,
+};
+store.clients['Oleksii Davydov'] = {
+    quantity: 4,
+    bonuses: 55,
+};
+store.clients['Olena Demchyk'] = {
+    quantity: 5,
+    bonuses: 75,
+};
+
+
+
